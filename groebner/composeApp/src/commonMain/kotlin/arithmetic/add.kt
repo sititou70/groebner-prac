d70b43e04024f8639fdc2e291f39a287e49f075a
Takuya Shizukuishi <sititou70@gmail.com>
2024-07-14 18:36:22 +0900

    refactor: add logic, rename file

--- groebner/composeApp/src/commonMain/kotlin/arithmetic/add.kt
+++ groebner/composeApp/src/commonMain/kotlin/arithmetic/add.kt
@@ -4,10 +4,5 @@ import types.Polynomial
 import types.polynomialOf
 
 fun addPolynomial(p1: Polynomial, p2: Polynomial): Polynomial {
-    var added = p2
-    for (p1Monomial in p1.monomials) {
-        added = polynomialOf(added.monomials + listOf(p1Monomial))
-    }
-
-    return added
+    return polynomialOf(p1.monomials + p2.monomials)
 }
